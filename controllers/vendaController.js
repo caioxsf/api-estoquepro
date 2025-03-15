00cdf22666ba3ce24774608c9bf6d6b7e25d4076
caioxsf <caio.nobile@hotmail.com>
2025-03-15 20:09:22 -0300

    fix:  se o código do produto estiver errado, ele gera a venda mas ao retornar 400 ele automaticamente exclui essa ultima venda gerada. eu sei que o melhor seria fazer o tal do rollbackm, mas eu ainda n sei :)

--- controllers/vendaController.js
+++ controllers/vendaController.js
@@ -13,46 +13,51 @@ export default class vendaController {
         this.#vendaRepo = new vendaRepository();
         this.#itensVendaRepo = new itensVendaRepository();
         this.#itensVendaEntity = itensVendaEntity;
-        this.#produtoRepository = new produtoRepository(); 
+        this.#produtoRepository = new produtoRepository();
     }
 
     async cadastrarVenda(req, res) {
-        
+
         // Gera a venda
         let vendaId = await this.#vendaRepo.gerarVenda();
         let cont = 0;
 
-        for (let i=0;i<req.body.length;i++) {
+        for (let i = 0; i < req.body.length; i++) {
             const entidade = new this.#itensVendaEntity();
-            let {quantidade, produto_id} = req.body[i];
+            let { quantidade, produto_id } = req.body[i];
 
-            // Busca o preço do produto pra fazer o subtotal direto no banco de dados
-            let resultado = await this.#produtoRepository.buscarPrecoDoProduto(produto_id)
-            let preco = resultado[0].prod_preco;
             // Verifica o código do produto pra ver se ele realmente existe
             entidade.produto_id = parseInt(produto_id);
-            if(await this.#itensVendaRepo.verificarCodigoDoProduto(produto_id)) {
+            if (await this.#itensVendaRepo.verificarCodigoDoProduto(produto_id)) {
+                // Busca o preço do produto pra fazer o subtotal direto no banco de dados
+                let resultado = await this.#produtoRepository.buscarPrecoDoProduto(produto_id)
+                let preco = resultado[0].prod_preco;
+                
                 entidade.venda_id = vendaId;
                 entidade.quantidade = parseInt(quantidade);
                 entidade.preco = parseFloat(preco);
                 entidade.subtotal = entidade.quantidade * preco;
                 // Verifica se o estoque do produto é maior que a quantidade desejada pelo cliente
                 // Se o retorno for diferente de null ele atualiza o estoque e cadastra a venda
-                if( await this.#produtoRepository.verificarEstoqueDoProduto(produto_id, quantidade)) {
+                if (await this.#produtoRepository.verificarEstoqueDoProduto(produto_id, quantidade)) {
                     await this.#itensVendaRepo.atualizarEstoque(entidade.quantidade, entidade.produto_id);
                     await this.#itensVendaRepo.cadastrarVenda(entidade);
                     cont++;
                 } else {
-                    return res.status(400).json({msg: `Estoque do produto ${produto_id} insuficiente!`});
-                }  
-            } else 
-                return res.status(404).json({msg: "Código do produto inexistente!"});
-        }  
+                    return res.status(400).json({ msg: `Estoque do produto ${produto_id} insuficiente!` });
+                }
+            } else {
+                await this.#vendaRepo.deletarVendaGerada(vendaId)
+                return res.status(404).json({ msg: "Código do produto inexistente!" });
+            }
+        }
         if (cont > 0)
-            return res.status(201).json({msg: "Pedido cadastrado com sucesso!"})
-        else 
-            return res.status(400).json({msg: "Erro ao cadastrar pedido"});
-            
+            return res.status(201).json({ msg: "Pedido cadastrado com sucesso!" })
+        else {
+            await this.#vendaRepo.deletarVendaGerada(vendaId)
+            return res.status(400).json({ msg: "Erro ao cadastrar pedido" });
+        }
+
     }
 
     // async alterarVenda(req,res) {
@@ -72,59 +77,59 @@ export default class vendaController {
     //     }
     // }
 
-    async listarVendas (req,res) {
+    async listarVendas(req, res) {
         let lista = await this.#itensVendaRepo.listarVendas();
-        if(lista.length === 0)
-            return res.status(404).json({msg: "Nenhuma venda foi encontrada!"});
-        return res.status(200).json(lista); 
+        if (lista.length === 0)
+            return res.status(404).json({ msg: "Nenhuma venda foi encontrada!" });
+        return res.status(200).json(lista);
     }
 
-    async excluirVendaUnica(req,res) {
-        let {id} = req.params;
-        if(await this.#itensVendaRepo.obterVendaUnica(id)) {
+    async excluirVendaUnica(req, res) {
+        let { id } = req.params;
+        if (await this.#itensVendaRepo.obterVendaUnica(id)) {
             let resultado = await this.#itensVendaRepo.excluirVendaUnica(id);
-            if(resultado)
-                return res.status(200).json({msg: "Venda excluida com sucesso!"});
-            else 
+            if (resultado)
+                return res.status(200).json({ msg: "Venda excluida com sucesso!" });
+            else
                 throw new Error("Erro ao excluir venda!");
         } else {
-            return res.status(404).json({msg: "Venda não encontrada!"});
+            return res.status(404).json({ msg: "Venda não encontrada!" });
         }
     }
 
-    async obterVendaUnica(req,res) {
-        let {id} = req.params;
+    async obterVendaUnica(req, res) {
+        let { id } = req.params;
         const lista = await this.#itensVendaRepo.obterVendaUnica(id) || [];
-        if(lista.length === 0)
-            return res.status(404).json({msg: "Venda não encontrada!"});
+        if (lista.length === 0)
+            return res.status(404).json({ msg: "Venda não encontrada!" });
         else
             return res.status(200).json(lista);
     }
 
-    async obterVendaInteira(req,res) {
-        let {id} = req.params;
+    async obterVendaInteira(req, res) {
+        let { id } = req.params;
         let lista = await this.#itensVendaRepo.obterVendaInteira(id) || [];
-        if(lista.length === 0)
-            return res.status(404).json({msg: "Vendas não encontrada!"});
+        if (lista.length === 0)
+            return res.status(404).json({ msg: "Vendas não encontrada!" });
         else
             return res.status(200).json(lista);
     }
 
-    async excluirVendaInteira(req,res) {
-        let {id} = req.params;
-        if(await this.#itensVendaRepo.obterVendaInteira(id)) {
+    async excluirVendaInteira(req, res) {
+        let { id } = req.params;
+        if (await this.#itensVendaRepo.obterVendaInteira(id)) {
             let resultado = await this.#itensVendaRepo.excluirVendaInteira(id);
-            if(resultado)
-                return res.status(200).json({msg: "Vendas excluidas com sucesso!"});
-            else 
+            if (resultado)
+                return res.status(200).json({ msg: "Vendas excluidas com sucesso!" });
+            else
                 throw new Error("Erro ao excluir vendas!");
         } else {
-            return res.status(404).json({msg: "Vendas não encontrada!"});
+            return res.status(404).json({ msg: "Vendas não encontrada!" });
         }
     }
 
-    
-    
+
+
 }
 
 

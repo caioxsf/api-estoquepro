cb9a8e8b47dde210ea837c6dea6cc27cc9b2b252
caioxsf <caio.nobile@hotmail.com>
2025-03-27 11:45:49 -0300

    fix: ajustando swagger.js

--- swagger.js
+++ swagger.js
@@ -4,7 +4,7 @@ const doc = {
     info: {
         title: "API RESTful",
     },
-    host: '152.67.55.245',
+    host: '152.67.55.245:5000',
     components: {
         schemas: {
             clientes: {
